3f837fe757c0bcb99b087cb5e2b556399547012a
Jgabriel <megagbl2323@gmail.com>
2025-01-18 18:10:12 -0400

    update title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,66 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="bg-white">
+    <main className="w-full min-h-screen flex flex-col md:flex-row items-center justify-center mx-auto">
+      {/* Login Section */}
+      <section className="w-full flex-1 flex flex-col justify-center items-center p-4">
+        <div className="w-full max-w-xl flex justify-center items-center flex-col gap-4">
+          <img 
+            src="./assets/user.png" 
+            alt="Logo do usuario"
+            className="w-36 hover:scale-110 duration-300"
+          />
+          <h1 className="text-4xl font-bold mb-7">Login</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <input 
+            type="text" 
+            placeholder="Digite o seu email..."
+            className="w-full p-2 rounded-lg outline-none bg-gray-100 focus:bg-white"
+          />
+
+          <input 
+            type="password" 
+            placeholder="************"
+            className="w-full p-2 rounded-lg outline-none bg-gray-100 focus:bg-white"
+          />
+
+          <div className="w-full flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              <input type="checkbox" id="remember" />
+              <label htmlFor="remember" className="font-medium">Lembrar senha</label>
+            </div>
+
+            <a href="#" className="text-sky-600 hover:text-sky-500 duration-200">Esqueceu a senha?</a>
+          </div>
+
+          <button className="w-full bg-blue-600 p-2 rounded-lg text-white text-lg font-medium hover:bg-blue-500 duration-200">
+            Acessar
+          </button>
+
+          <p>
+            Não possui uma conta?{' '}
+            <a href="#" className="font-medium text-sky-600 hover:text-sky-500 duration-200">Criar uma conta</a>
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </section>
+
+      {/* Info Section */}
+      <section 
+        className="hidden md:flex gap-9 w-full flex-1 bg-blue-600 h-screen items-center justify-center flex-col p-4"
+      >
+        <h1 className="text-white text-2xl text-center max-w-lg lg:text-4xl">
+          Domine as <strong>tecnologias mais buscadas</strong> pelo mercado!
+        </h1>
+
+        <img
+          src="assets/techs.png"
+          alt="Tecnologias usadas no mercado"
+          className="w-3/4 lg:w-full lg:max-w-md"
+        />
+      </section>
+    </main>
+  </div>
+  )
+
 }
